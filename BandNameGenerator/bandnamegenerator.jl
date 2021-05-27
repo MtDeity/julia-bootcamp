aacad1e27ed5ee4a7961256e493b4bf593e0a8a1
Satoshi Kamiyama <55924931+MtDeity@users.noreply.github.com>
2021-05-28 00:17:18 +0900

    Band Name Generator

---
+++ BandNameGenerator/bandnamegenerator.jl
@@ -0,0 +1,6 @@
+println("Welcome to the Band Name Generator.")
+println("What's name of the city you grew up in?")
+city = readline()
+println("What's your pet's name?")
+pet = readline()
+println("Your band name could be $city $pet")
